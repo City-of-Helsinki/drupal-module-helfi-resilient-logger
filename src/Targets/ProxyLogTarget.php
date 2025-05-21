326c94891c09b796264d05d9032cfbf7d6c98078
Kalle Jokinen <kalle.jokinen@leasedealit.fi>
2025-05-21 18:07:15 +0300

    refactor: rename proxy log target class.

--- src/Targets/ProxyLogTarget.php
+++ src/Targets/ProxyLogTarget.php
@@ -1,13 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Drupal\helfi_resilient_logger\Targets;
 
 use ResilientLogger\Sources\AbstractLogSource;
 use ResilientLogger\Targets\AbstractLogTarget;
 use \Psr\Log\LoggerInterface;
 
-class DrupalProxyLogTarget extends AbstractLogTarget {
+class ProxyLogTarget extends AbstractLogTarget {
   private const DEFAULT_LOGGER_NAME = "resilient_logger";
+
   private LoggerInterface $logger;
 
   public function __construct(array $options) {
