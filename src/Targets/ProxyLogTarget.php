a1b76de46280145595db5c3309d551708e8fe590
Kalle Jokinen <kalle.jokinen@leasedealit.fi>
2025-05-22 14:20:33 +0300

    refactor: avoid inline hard-coded strings.

--- src/Targets/ProxyLogTarget.php
+++ src/Targets/ProxyLogTarget.php
@@ -9,6 +9,7 @@ use ResilientLogger\Targets\AbstractLogTarget;
 use \Psr\Log\LoggerInterface;
 
 class ProxyLogTarget extends AbstractLogTarget {
+  private const OPTION_LOGGER_NAME = "name";
   private const DEFAULT_LOGGER_NAME = "resilient_logger";
 
   private LoggerInterface $logger;
@@ -16,8 +17,8 @@ class ProxyLogTarget extends AbstractLogTarget {
   public function __construct(array $options) {
     parent::__construct($options);
 
-    $name = array_key_exists("name", $options)
-      ? $options["name"]
+    $name = array_key_exists(self::OPTION_LOGGER_NAME, $options)
+      ? $options[self::OPTION_LOGGER_NAME]
       : self::DEFAULT_LOGGER_NAME;
     
     $this->logger = \Drupal::logger($name);
