6ce8d9f32cce11b38b47b8844b7811d23ed45474
Kalle Jokinen <kalle.jokinen@leasedealit.fi>
2025-09-25 14:25:30 +0300

    feat: update to newer php-resilient-logger

---
+++ src/ResilientLogger.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\helfi_resilient_logger;
+
+use ResilientLogger\ResilientLogger as ResilientLoggerBase;
+use Drupal\Core\Site\Settings;
+use ResilientLogger\Types;
+
+/**
+ * @phpstan-import-type ResilientLoggerOptions from Types
+ */
+class ResilientLogger extends ResilientLoggerBase {
+  public static function createFromSettings(Settings $settings): self {
+    /** @var ResilientLoggerOptions $options */
+    $options = $settings->get('resilient_logger', []);
+
+    return parent::create($options);
+  }
+}
+
+?>
